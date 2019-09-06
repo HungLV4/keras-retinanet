79e1f1a4e050aeb83c5a3ee0bde1e3a9d65a375f
Lưu Hưng <hunglv@fimo.edu.vn>
2019-09-06 19:16:06 +0700

    debug

--- keras_retinanet/losses.py
+++ keras_retinanet/losses.py
@@ -63,10 +63,10 @@ def focal(alpha=0.25, gamma=2.0):
         # fill alpha_factor with zeros to used as mixup labels
         mixup_labels = keras.backend.zeros_like(labels)
 
-        # cls_loss     = focal_weight * (keras.backend.binary_crossentropy(labels, classification) * lams + 
-        #                  keras.backend.binary_crossentropy(mixup_labels, classification) * (1 - lams))
+        cls_loss     = focal_weight * (keras.backend.binary_crossentropy(labels, classification) * lams + 
+                         keras.backend.binary_crossentropy(mixup_labels, classification) * (1 - lams))
 
-        cls_loss     = focal_weight * keras.backend.binary_crossentropy(labels, classification)
+        # cls_loss     = focal_weight * keras.backend.binary_crossentropy(labels, classification)
 
         # compute the normalizer: the number of positive anchors
         normalizer = backend.where(keras.backend.equal(anchor_state, 1))
