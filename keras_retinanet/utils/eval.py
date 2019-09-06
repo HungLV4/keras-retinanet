e3e629789142b88a205d50fa3d966e73bf758305
Lưu Hưng <hunglv@fimo.edu.vn>
2019-09-06 17:13:25 +0700

    fix eval draw image

--- keras_retinanet/utils/eval.py
+++ keras_retinanet/utils/eval.py
@@ -74,7 +74,7 @@ def _get_detections(generator, model, score_threshold=0.05, max_detections=100, 
 
     # for i in progressbar.progressbar(range(generator.size()), prefix='Running network: '):
     for i in range(generator.size()):
-        raw_image, raw_lam  = generator.load_image(i)
+        raw_image, raw_lam  = generator.load_bgr_image(i)
         image               = generator.preprocess_image(raw_image.copy())
         image, scale        = generator.resize_image(image)
 
