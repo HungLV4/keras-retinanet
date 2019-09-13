74648dfaa2221052768d497c14d402849531c7fa
Lưu Hưng <hunglv@fimo.edu.vn>
2019-09-13 10:19:29 +0700

    set nms_threshold to 0.1

--- keras_retinanet/layers/filter_detections.py
+++ keras_retinanet/layers/filter_detections.py
@@ -120,7 +120,7 @@ class FilterDetections(keras.layers.Layer):
         self,
         nms                   = True,
         class_specific_filter = True,
-        nms_threshold         = 0.2,
+        nms_threshold         = 0.1,
         score_threshold       = 0.05,
         max_detections        = 300,
         parallel_iterations   = 32,
